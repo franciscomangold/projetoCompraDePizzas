1696d309de697c26e31dd7589aa2c12a7b72d05c
Francisco Mangold <franciscomangold@gmail.com>
2020-07-14 22:09:23 -0300

    Adicionando os nomes das pizzas na página

--- pizzas.js
+++ pizzas.js
@@ -1,3 +1,13 @@
+/*
+*
+* json - JavaScript Object Notation
+* Notação de Objetos JavaScript
+* É uma formatação leve de troca de dados. Para seres humanos,
+* é fácil de ler e escrever.
+*
+*/
+
+
 let pizzaJson = [
     {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
--- script.js
+++ script.js
@@ -8,17 +8,27 @@
 *
 */
 
-// Passo 1, mapeando a lista
+/*      Passo 1, mapeando a lista
+* 
+*
+* Criando uma função (const c e cs), que vai receber um elemento(el) para substituir document.querySelector/SelectorALL()
+* append() - Função para adicionar.
+* O innerHTML não adiciona e sim substitui o item, por isso está sendo usado o append(). 
+*/
 
-// Criando uma função (const c e cs), que vai receber um elemento(el) para substituir document.querySelector/SelectorALL()
 const c = (el)=>document.querySelector(el);      //Retorna um item
 const cs = (el)=>document.querySelectorAll(el);  //Retorna todo o array
 
-pizzaJson.map((item, index)=>{  // recebendo item(cada pizza, começando do zero), e o index(posição, começando do zero).
+pizzaJson.map((item, index)=>{  // recebendo item(cada pizza), e o index(posição, começando do zero(id:1 é o zero, id:2 é um ...)).
     
-    let pizzaItem = c('.models .pizza-item').cloneNode(true);
-    // preencher as informações em pizzasitem
+    let pizzaItem = c('.models .pizza-item').cloneNode(true); // Clonando o item(pizza-item) e salvando na variável pizzaItem.
 
-    c('.pizza-area').append(pizzaItem);
+    // preenchendo as informações em pizzasitem
+
+
+    // Adicionando o nome das pizzas
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+
+    c('.pizza-area').append(pizzaItem); // Adicionando elemento(el)
 });
 
